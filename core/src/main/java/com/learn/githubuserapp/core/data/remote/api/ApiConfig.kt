5e93516aab4e721e6663d7f78f50e2c787f1cad8
Fardal <80554545+fardall@users.noreply.github.com>
2023-05-23 13:26:14 +0700

    Add certificate pinner

--- core/src/main/java/com/learn/githubuserapp/core/data/remote/api/ApiConfig.kt
+++ core/src/main/java/com/learn/githubuserapp/core/data/remote/api/ApiConfig.kt
@@ -2,6 +2,7 @@ package com.learn.githubuserapp.core.data.remote.api
 
 import androidx.viewbinding.BuildConfig
 import com.learn.githubuserapp.core.utils.BASE_API_URL
+import okhttp3.CertificatePinner
 import okhttp3.OkHttpClient
 import okhttp3.logging.HttpLoggingInterceptor
 import retrofit2.Retrofit
@@ -10,11 +11,17 @@ import retrofit2.converter.gson.GsonConverterFactory
 class ApiConfig {
     companion object{
         fun getApiService(): ApiService {
+            val hostname = "api.github.com"
             val loggingInterceptor = if(BuildConfig.DEBUG) {
                 HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.BODY)
             } else {
                 HttpLoggingInterceptor().setLevel(HttpLoggingInterceptor.Level.NONE)
             }
+            val certificatePinner = CertificatePinner.Builder()
+                .add(hostname, "sha256/1UPHAdcUbUoOcd5rDTD/0oMSnngCU6YzXzpByO4CCp4=")
+                .add(hostname, "sha256/RQeZkB42znUfsDIIFWIRiYEcKl7nHwNFwWCrnMMJbVc=")
+                .add(hostname, "sha256/Jg78dOE+fydIGk19swWwiypUSR6HWZybfnJG/8G7pyM=")
+                .build()
             val client = OkHttpClient.Builder()
                 .addInterceptor { chain ->
                     val newRequest = chain.request().newBuilder()
@@ -22,6 +29,7 @@ class ApiConfig {
                     chain.proceed(newRequest)
                 }
                 .addInterceptor(loggingInterceptor)
+                .certificatePinner(certificatePinner)
                 .build()
             val retrofit = Retrofit.Builder()
                 .baseUrl(BASE_API_URL)
